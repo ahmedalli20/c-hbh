0cbe99b46080e197b2b4069014f1fda713e249ce
kahoragachau <joelkahoragachau6@gmail.com>
2022-11-09 19:00:40 +0300

    - Complete Loginform Design

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,50 @@ import './LoginForm.css'
 
 function LoginForm(){
     return(
-        <p>Hello, World</p>
+        <>
+        <div className='form-container'>
+            <div className='form-img'>
+                <img src='https://www.thewhiskeyshelf.com/wp-content/uploads/2019/04/EHT-Single-Barrel-Front-Compressed.jpg'/>
+            </div>
+            <div className='form-details'>
+                <h2 className='text-center text-white'>Login</h2>
+                <p className='text-center text-white'>Welcome Back!</p>
+                <form className="forms">
+                        <div className="col-md-12 mb-3">
+                            <label
+                            for="inputUsername"
+                            className="form-label text-white">
+                                Username
+                            </label>
+                            <input
+                            type="text"
+                            className="form-control col-md-6"
+                            id="inputEmail"
+                            />
+                        </div>
+                        <div className="col-md-12 mb-3">
+                            <label
+                            for="inputPassword"
+                            className="form-label text-white">
+                                Password
+                            </label>
+                            <input
+                            type="password"
+                            className="form-control"
+                            id="inputPassword"
+                            width="50px"
+                            />
+                        </div>
+                        <button
+                        type="submit"
+                        className="submit-btn">
+                            Log In
+                        </button>
+                </form>
+                    <p className="mt-2 text-white">Don't have an account? <span><a href="/">Sign Up</a></span> Here</p>
+            </div>
+        </div>
+        </>
     )
 }
 
