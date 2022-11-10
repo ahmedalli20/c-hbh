69a73ccd6e2f70038aa93f4ed102b8aa99b0cf74
kahoragachau <joelkahoragachau6@gmail.com>
2022-11-10 19:23:27 +0300

    - Set Formdata

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,8 +1,22 @@
+import { useState } from 'react';
 import './SignupForm.css';
 
 function SignupForm(){
-    const handleSignin = () => {
-        alert("Clicked!")
+    // const handleSignin = (event) => {
+    //     event.preventDefault();
+    //     alert("Submitted!")
+    // }
+
+    const [formData, setFormData] = useState({
+        user_name:"",
+        email: "",
+        password_digest: ""
+        })
+
+    function handleSignin(event){
+        event.preventDefault();
+        console.log(formData)
+        alert("Submitted")
     }
     return(
         <>
@@ -13,7 +27,7 @@ function SignupForm(){
             <div className='form-details'>
                 <h2 className='text-center text-white'>Sign Up</h2>
                 {/* <p className='text-center text-white'>Welcome Back!</p> */}
-                <form className="forms">
+                <form className="forms" onSubmit={handleSignin}>
                         <div className="col-md-12 mb-3">
                             <label
                             for="inputUsername"
@@ -23,7 +37,9 @@ function SignupForm(){
                             <input
                             type="text"
                             className="form-control col-md-6"
-                            id="inputEmail"
+                            id="inputUsername"
+                            onChange={(event) => setFormData({...formData, user_name:event.target.value})}
+                            value={formData.user_name}
                             />
                         </div>
                         <div className="col-md-12 mb-3">
@@ -36,6 +52,8 @@ function SignupForm(){
                             type="email"
                             className="form-control col-md-6"
                             id="inputEmail"
+                            onChange={(event) => setFormData({...formData, email:event.target.value})}
+                            value={formData.email}
                             />
                         </div>
                         <div className="col-md-12 mb-3">
@@ -49,6 +67,8 @@ function SignupForm(){
                             className="form-control"
                             id="inputPassword"
                             width="50px"
+                            onChange={(event) => setFormData({...formData, password_digest:event.target.value})}
+                            value={formData.password_digest}
                             />
                         </div>
                         <div className="col-md-12 mb-3">
@@ -62,12 +82,14 @@ function SignupForm(){
                             className="form-control"
                             id="inputPassword"
                             width="50px"
+                            // onChange={(event) => event.target.value}
+                            // value={event.target.value}
                             />
                         </div>
                         <button
                         type="submit"
-                        className="submit-btn" onClick={()=> handleSignin()}>
-                            Log In
+                        className="submit-btn">
+                            Sign Up
                         </button>
                 </form>
                 <p className="mt-2 text-white">Already have an account <span><a href="/">Sign Up</a></span></p>
