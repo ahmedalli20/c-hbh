6ffc72c3821d6f5a15d221998308aae47297c4dd
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-11-10 12:28:34 +0300

    ft: add single order component

--- src/components/orderComponent/OrderDetails.js
+++ src/components/orderComponent/OrderDetails.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import SingleOrder from './SingleOrder'
+import React from 'react';
+import SingleOrder from './SingleOrder';
+import image from '../../assets/images/budweiser_small.png';
 
 function OrderDetails() {
     const styles={
@@ -7,12 +8,24 @@ function OrderDetails() {
         height: '80vh',
         borderRadius: 10,
     }
+    const beer = "Budweiser Root Beer";
+    const price = 203.50;
+    const pieces = 4091;
     return (
         <div className='container' style={styles}>
             <h5 className='text-start p-3'>
                 Order Details
-            </h5>  
-            <SingleOrder/>           
+            </h5>
+
+            <div className='container d-flex justify-content-center align-items-center flex-column'>
+                <SingleOrder image={image} beer={beer} price={price} pieces={pieces}/>
+                <SingleOrder image={image} beer={beer} price={price} pieces={pieces}/>
+                <SingleOrder image={image} beer={beer} price={price} pieces={pieces}/>
+                <SingleOrder image={image} beer={beer} price={price} pieces={pieces}/>
+                <SingleOrder image={image} beer={beer} price={price} pieces={pieces}/>
+                <SingleOrder image={image} beer={beer} price={price} pieces={pieces}/>
+
+            </div>
         </div>
     )
 }
--- src/components/orderComponent/SingleOrder.js
+++ src/components/orderComponent/SingleOrder.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
-function SingleOrder() {
+function SingleOrder({ image, beer, price, pieces }) {
     return (
-        <div className='container d-flex justify-content-center align-items-center'>
-            <div className='bg-warning' style={{ width: '50%', borderRadius: 5, height: '40px' }}>awesome</div>
-        </div>
+        <>
+            <div className='bg-warning d-flex justify-content-evenly align-items-center m-2 text-center' style={{ width: '70%', borderRadius: 5, height: '40px' }}>
+                <div className=''><img src={image} style={{ height: '40px', width: 'auto', padding: '5px' }} /></div>
+                {/* <div> */}
+                <div><h6>{beer}</h6></div>
+                <h6>{price}</h6>
+                <h6>{pieces}</h6>
+                {/* </div> */}
+                <div>
+                    <button className='m-1' style={{ backgroundColor: "maroon", width: '50px', border: 'none', color: "#f2f2f2", borderRadius: '5px' }}>-</button>
+                    <button className='m-1' style={{ backgroundColor: "maroon", width: '50px', border: 'none', color: "#f2f2f2", borderRadius: '5px' }}>+</button>
+                </div>
+            </div>
+        </>
     )
 }
 
