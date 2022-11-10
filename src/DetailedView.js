5169f6e5d01bf064c8235fc25330d47040076124
Anne Ogongo <anneogongo98@gmail.com>
2022-11-10 05:32:06 +0300

    home components structure

---
+++ src/DetailedView.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function DetailedView({name, category, price, description}) {
+    return (
+        <div>
+            <img src=""/>
+            <P></P>
+            <P></P>
+            <p></p>
+            <button></button>
+        </div>
+
+    )
+}
+
+export default DetailedView;
