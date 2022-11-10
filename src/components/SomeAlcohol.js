7ce481bbc6d6bce46809a7770a4433ef42519bd0
Anne Ogongo <anneogongo98@gmail.com>
2022-11-11 00:23:35 +0300

    Detailed View

--- src/components/SomeAlcohol.js
+++ src/components/SomeAlcohol.js
@@ -2,7 +2,7 @@ import React from "react";
 import DetailedView from "./DetailedView";
 
 function SomeAlcohol({data}){
-    const some = data.slice(0,6)
+    const some = data.slice(0,1)
 
     const alcohol = some.map((item) => <DetailedView key={item.id} name={item.name} category_name={item.category_name} price={item.price} image={item.image} quantity={item.quantity} description={item.description}/>)
 
