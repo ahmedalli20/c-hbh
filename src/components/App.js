94c72a285126f55ef74f4be792cb060079e57579
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-11-11 04:03:55 +0300

    fix detailed view

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,12 @@
 import '../assets/App.css';
-import Banner from './navigation/Banner';
-import Navbar from './navigation/NavBar';
-import Checkout from './checkoutComponent/Checkout';
-import OrderDetails from './orderComponent/OrderDetails'
-import Dashboard from './dashboardComponents/Dashboard';
 import LoginForm from './LoginForm'
-import Home from './Home';
-import LandingPage from './Landing_page';
-import LoginForm from './LoginForm';
-import Checkout from './checkoutComponent/Checkout';
-import SignupForm from './SignupForm';
 import DetailedView from './DetailedView';
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
-     {/* <LoginForm /> */}
-     {/* <LandingPage/> */}
-     {/* <Home/> */}
+      {/* <LoginForm /> */}
      <DetailedView />
-     {/* <SignupForm /> */}
     </div>
   );
 }
--- src/components/DetailedView.js
+++ src/components/DetailedView.js
@@ -5,9 +5,11 @@ function DetailedView({ name, category_name, price, description, image, quantity
     return (
         <div className="detail">
             <h4>Product Details</h4>
-            <div className="d-flex justify-content-center align-items-center
+            <div className="d-flex justify-content-around align-items-center
 " >
-                <img src={BeerImage} alt="beer" className="beer" />
+                <div className="beer-container d-flex justify-content-center ">
+                    <img src={BeerImage} alt="beer" className="beer" />
+                </div>
                 <div className="details">
                     <p className="name">{name}</p>
                     <p className="category">Category:<span>{category_name}</span> </p>
